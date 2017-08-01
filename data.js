1ffce703254ed33050e0177924e3f4cacd03385b
tamrat teare <tamrat33@gmail.com>
2017-07-31 21:30:46 -0400

    add the data js and final commit

---
+++ data.js
@@ -0,0 +1,6 @@
+  const chalk = require('chalk');
+   const moment = require('moment')
+    let now = moment("Jul 18, 2013").format('MMM. d, YYYY');
+     console.log(`It is ${now}.`);
+  module.exports = now;
+  Contact GitHub API Training Shop Blog About
--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 const moment = require('moment');
 const chalk = require('chalk');
 
-console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
-console.log(chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
+let line1 = chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+let line2 = chalk.magenta(moment().format("DDDo"));
+let line3 = chalk.cyan(moment().seconds() + moment().minutes() * 60 + moment().hour() * 3600);
+function isDaylightSavingsTime() {
+  let line4 = moment().isDST();
+  if (line4 === true) {
+    console.log('It ' + chalk.green('is') + ' during Daylight Savings Time.');
+  } else {
+    console.log('It ' + chalk.green('is not') + ' during Daylight Savings Time.');
+  }
+}
+function isNowLeapYear() {
+  let line5 = moment().isLeapYear();
+  if (line5 === true) {
+    console.log('It ' + chalk.red('is') + ' a leap year.');
+  } else {
+    console.log('It ' + chalk.red('is not') + ' a leap year.');
+  }
+}
+console.log(`It is ${line1}.`);
+console.log(`It is the ${line2} day of the year.`);
+console.log(`It is ${line3} seconds into the day.`);
+`${isDaylightSavingsTime()}`;
+`${isNowLeapYear()}.`;
